1ef4bad466f2547b174bbce5260198942eea649f
ChenXj6 <2953076676@qq.com>
2021-01-05 18:22:59 +0800

    124

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ import store from './store'
 
 import filter from './common/filter/filter'
 import mixin from './common/mixin/mixin'
-Vue.mixin(mixin) 
+
+Vue.mixin(mixin)
 
 
 Vue.prototype.$store = store
@@ -15,12 +16,15 @@ Vue.config.productionTip = false
 // 注册全局组件
 import MescrollBody from "@/components/mescroll-uni/mescroll-body.vue"
 import MescrollUni from "@/components/mescroll-uni/mescroll-uni.vue"
+import uniIcons from "@/components/uni-icons/uni-icons"
+
+Vue.component('uni-icons', uniIcons)
 Vue.component('mescroll-body', MescrollBody)
 Vue.component('mescroll-uni', MescrollUni)
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
---
+++ pages/index/brandList/brandList.vue
@@ -0,0 +1,101 @@
+<template>
+	<view class="content">
+		<view class="brandListBox">
+			<p>推荐品牌</p>
+			<view class="brandList">
+				<view class="brand radio">
+					<image src="/static/images/首页/u234.png" mode=""></image>
+					<p>东风</p>
+				</view>
+				<view class="brand radio">
+					<image src="/static/images/首页/u234.png" mode=""></image>
+					<p>东风</p>
+				</view>
+				<view class="brand radio">
+					<image src="/static/images/首页/u234.png" mode=""></image>
+					<p>东风</p>
+				</view>
+				<view class="brand radio">
+					<image src="/static/images/首页/u234.png" mode=""></image>
+					<p>东风</p>
+				</view>
+				<view class="brand radio">
+					<image src="/static/images/首页/u234.png" mode=""></image>
+					<p>东风</p>
+				</view>
+				<view class="brand radio">
+					<image src="/static/images/首页/u234.png" mode=""></image>
+					<p>东风</p>
+				</view>
+				<view class="brand radio">
+					<image src="/static/images/首页/u234.png" mode=""></image>
+					<p>东风</p>
+				</view>
+				<view class="brand radio">
+					<image src="/static/images/首页/u234.png" mode=""></image>
+					<p>东风</p>
+				</view>
+				
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+	.radio{
+		border-radius: 10rpx;
+	}
+	.brandListBox{
+		width: 100%;
+		padding: 20rpx;
+		height: 600rpx;
+		color: #333333;
+		box-sizing: border-box;
+		>p{
+			font-weight: 400;
+			font-size: 26rpx;
+			height: 32rpx;
+			line-height: 32rpx;
+			margin:20rpx 10rpx ;
+			font-family: 'Arial Normal','Arial';
+			font-style: normal;
+		}
+		>.brandList{
+			width: 100%;
+			height: 100%;
+			padding: 0 10rpx;
+			box-sizing: border-box;
+			display: flex;
+			// justify-content: space-around;
+			flex-wrap: wrap;
+			align-content: flex-start;
+			>.brand{
+				width: 192rpx;
+				height: 140rpx;
+				padding-top: 20rpx;
+				margin: 10rpx 15rpx;
+				background-color: #FFFFFF;
+				font-size: 26rpx;
+				text-align: center;
+				display: flex;
+				align-items: center;
+				flex-direction: column;
+				justify-content: space-around;
+				>image{
+					width: 114rpx;
+					height: 68rpx;
+				}
+			}
+		}
+	}
+</style>
